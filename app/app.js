3000928df0da38ac2d29ec7f1f93ee89d09d0850
alessio <pc_alessio@hotmail.com>
2014-02-23 15:24:49 +0000

    form and congrats pages

--- app/app.js
+++ app/app.js
@@ -8,6 +8,8 @@
     .config(['$routeProvider', function($routeProvider) {
       $routeProvider.when('/services', {templateUrl: 'partials/services.html', controller: "Services"});
       $routeProvider.when('/from', {templateUrl: 'partials/from.html'});
+      $routeProvider.when('/form', {templateUrl: 'partials/form.html'});
+      $routeProvider.when('/congrats', {templateUrl: 'partials/congrats.html'})
       $routeProvider.otherwise({redirectTo: 'from'});
     }]);
 })();
